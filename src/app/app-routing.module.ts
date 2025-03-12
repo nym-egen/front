6ca84cd59744a2aa99c760234592513edffb1fa8
Md Nayeem Hossain <nayeem.hossain@egeneration.co>
2025-03-12 14:31:45 +0600

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CustomerListComponent} from './customer/customer-list/customer-list.component';
+import {CustomerAddComponent} from './customer/customer-add/customer-add.component';
+import {ItemAddComponent} from "./item/item-add/item-add.component";
+import {ItemListComponent} from "./item/item-list/item-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component: CustomerListComponent},
+  {path: 'customer-add', component: CustomerAddComponent},
+  {path: 'item-add', component: ItemAddComponent},
+  {path: 'item-list', component: ItemListComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerAddComponent } from './customer/customer-add/customer-add.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { RouterModule } from '@angular/router';
+import { ItemAddComponent } from './item/item-add/item-add.component';
+import { ItemListComponent } from './item/item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerAddComponent,
+    CustomerListComponent,
+    ItemAddComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
